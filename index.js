6b5cce9c7e855ee5cd1fb7c787fa6f5a5f4fc275
LombreBlanche34 <54815949+LombreBlanche34@users.noreply.github.com>
2020-05-12 15:31:15 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const discord = require('discord.js')
-const client = new discord.Client()
-
-client.on('ready', () => {
-    console.log('pret !')
-    client.user.setActivity("%help", {type:"WATCHING"})
-})
-
-client.login("Y")
